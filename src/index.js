31b7ee47215a400851b1c3a1b8460e276093b8cb
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-08-25 13:53:31 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    form.reset()
+
+    // adding properties
+    let li = document.createElement('li')
+    let userInput = document.getElementById("new-task-description")
+    li.textContent = userInput.value
+    let ul = document.querySelector('#tasks')
+    ul.appendChild(li)
+
+    // creating a remove button
+    let btn = document.createElement('button')
+    btn.textContent = 'X'
+    li.appendChild(btn)
+    btn.addEventListener("click", () => {li.remove()}) 
+  });
+
+})
